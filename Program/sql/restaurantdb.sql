2d8cd0f46f75aae2ccf4fb3fb14cfa00609092e1
RafaelHMachado <rafael_henriquemachado@hotmail.com>
2019-03-31 20:18:50 -0700

    restaurant table created

--- Program/sql/restaurantdb.sql
+++ Program/sql/restaurantdb.sql
@@ -1,3 +1,9 @@
-CREATE TABLE `TABLE` (
-
+CREATE TABLE Restaurant ( 
+    RestaurantId INT AUTO_INCREMENT NOT NULL,
+    Name CHAR(50) NOT NULL,
+    TimeOpen VARCHAR(50) NOT NULL,
+    TimeClose VARCHAR(50) NOT NULL,
+    DayOpen VARCHAR(50) NOT NULL,
+    DayClose VARCHAR(50) NOT NULL,
+    PRIMARY KEY (RestaurantId)
 );
